f0b4b7eaf64b8d45d55479b39ac98babe3efa50e
nishalk01 <nishalk25121999@gmail.com>
2021-08-02 22:59:22 +0530

    adding body

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
-var i = 0;
 
-const rando=["som1","som2","good"]
-function typeWriting(txt){
-    if(i< txt.length){
-        
-        document.getElementById("typed-text").innerHTML+=txt.charAt(i);
-        i++;
-        setTimeout(()=>{
-             setTimeout(typeWriting(txt),2000)
-            
-        },1000)
-        
-    }
+// display all skills
+const logo_list=document.getElementById("logo-text-container");
+console.log(logo_list)
+
+let logo_with_name = [["HTML5", "./assets/HTML5_logo_and_wordmark.svg"], 
+                      ["BOOTSTRAP","./assets/botsrap(1).png"],
+                      ["MATERIALIZECSS","./assets/materializecss.png"],
+                      ["CSS3","./assets/css3.svg"],
+                      ["REACTJS",'./assets/reactJS.png'],
+                      ["REACT NATIVE","./assets/reactNative.png"],
+                      ["DJANGO",'./assets/django-logo.png'],
+                      ['NODEJS',"https://nodejs.org/static/images/logos/nodejs-new-pantone-black.svg"],
+                      ['MONGODB','./assets/mongodb.png']
+                    ];
+
+
+
+for (let i=0;i<logo_with_name.length;i++){
+    console.log("hello i passed")
+  logo_list.insertAdjacentHTML('beforeend', `
+  <div>
+  <img src=${logo_with_name[i][1]}   width="90" height="90" style="margin:10px;" alt=${logo_with_name[i][0]}  > 
+                            <div>${logo_with_name[i][0]}</div>
+                            </div>
+  `);
 }
-
-// setTimeout(typeWriting(),);
-
-typeWriting('text')
