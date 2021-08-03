1c02b4c5b82b078e12bd5ebcd2c665179bf5182e
nishalk01 <nishalk25121999@gmail.com>
2021-08-03 20:43:25 +0530

    added card 📇 and fixed-skill-logo

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
 
 // display all skills
 const logo_list=document.getElementById("logo-text-container");
-console.log(logo_list)
 
 let logo_with_name = [["HTML5", "./assets/HTML5_logo_and_wordmark.svg"], 
-                      ["BOOTSTRAP","./assets/botsrap(1).png"],
-                      ["MATERIALIZECSS","./assets/materializecss.png"],
                       ["CSS3","./assets/css3.svg"],
-                      ["REACTJS",'./assets/reactJS.png'],
-                      ["REACT NATIVE","./assets/reactNative.png"],
-                      ["DJANGO",'./assets/django-logo.png'],
-                      ['NODEJS',"https://nodejs.org/static/images/logos/nodejs-new-pantone-black.svg"],
-                      ['MONGODB','./assets/mongodb.png']
+                      ["Bootstrap","./assets/botsrap(1).png"],
+                      ["ReactJS",'./assets/reactJS.png'],
+                      ["ReactNative","./assets/reactNative.png"],
+                      ["Django",'./assets/django-logo.png'],
+                      ['NodeJS',"https://nodejs.org/static/images/logos/nodejs-new-pantone-black.svg"],
+                      ['MongoDB','./assets/mongodb.png'],
+                      ["Materializecss","./assets/materializecss.png"],
                     ];
 
 
 
 for (let i=0;i<logo_with_name.length;i++){
-    console.log("hello i passed")
   logo_list.insertAdjacentHTML('beforeend', `
   <div>
-  <img src=${logo_with_name[i][1]}   width="90" height="90" style="margin:10px;" alt=${logo_with_name[i][0]}  > 
-                            <div>${logo_with_name[i][0]}</div>
+  <img src=${logo_with_name[i][1]}   width="75" height="75"  alt=${logo_with_name[i][0]}  > 
+                            <div style="font-size:17px;">${logo_with_name[i][0]}</div>
                             </div>
   `);
+}
+
+const line=document.getElementById("line");
+for(let i=1; i<120;i++){
+    line.insertAdjacentHTML('beforeend',
+    `<div>${i}</div>`
+    )  
 }
